9f3948ca1abdbe3f6455d9a933ae14c6fc0aa6fc
fgokeke <okekefeargod@gmail.com>
2024-04-12 11:50:49 +0000

    9. Hoisting 4

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-classes-per-file */
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -14,7 +12,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -30,14 +27,13 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
