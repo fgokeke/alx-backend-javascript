55331079ffe8531f974d165646a259f5508a5337
okekefeargod <okekefeargod@gmail.com>
2024-06-05 02:14:49 +0000

    1. Using Process stdin 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,13 @@
-// Import the readline module to handle input from the command line
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface to read lines from the standard input
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const uname = process.stdin.read();
+
+  if (uname) {
+    process.stdout.write(`Your name is: ${uname}`);
+  }
 });
 
-// Display the welcome message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Handle the input from the user
-r1.on('line', (input) => {
-  // Display the user's input
-  console.log(`Your name is: ${input}`);
-
-  // Close the readline interface
-  rl.close();
-});
-
-// Handle the close event to display the closing message
-r1.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
